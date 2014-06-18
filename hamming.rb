a13dded8a02946f425d9fcfee31b39f3ca83ee66
Luke McComb <lucasmccomb@gmail.com>
2014-06-18 16:06:02 -0600

    completed hamming ex

--- hamming.rb
+++ hamming.rb
@@ -1,7 +1,31 @@
 require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
-
+ if strand_1.length == strand_2.length && strand_1 != strand_2
+    hamm = []
+    strand_1_arry = strand_1.split("")
+    strand_2_arry = strand_2.split("")
+    hamm = strand_1_arry - strand_2_arry
+      if (strand_1_arry - strand_2_arry) == strand_1_arry
+        strand_1.length
+      else
+        hamm.length
+      end
+ elsif (strand_1.length - strand_2.length) >= 0
+   pop_diff = strand_1.length - strand_2.length
+   strand_2_arry = strand_2.split("")
+   strand_1_arry = strand_1.split("")
+   strand_1_arry.pop(pop_diff)
+   hamm = strand_1_arry - strand_2_arry
+   hamm.length
+ elsif (strand_2.length - strand_1.length) >= 0
+   pop_diff = strand_2.length - strand_1.length
+   strand_2_arry = strand_2.split("")
+   strand_1_arry = strand_1.split("")
+   strand_2_arry.pop(pop_diff)
+   hamm = strand_2_arry - strand_1_arry
+   hamm.length
+ end
 end
 
 check("Hamming distance between identical strands",
@@ -24,3 +48,7 @@ check("Ignoring extra length in the first strand when it's longer",
 
 check("Ignoring extra length in the second strand when it's longer",
       hamming("AAA", "AAAG") == 0)
+
+# Figure out the number of differences the two strings have
+# loop through each string character by character to determine actual number of differences
+# Print a number representing the number of differences between the two strands
